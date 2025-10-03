3b2771095796be45bad231fb16c5bb1a39157325
rdz <l22310532@lapaz.tecnm.mx>
2025-10-02 21:11:27 -0700

    Update CSV headers to include mean values for IMU and PPG features

--- python recepcion/recepcionDatosEnergiaEspectral.py
+++ python recepcion/recepcionDatosEnergiaEspectral.py
@@ -2,14 +2,14 @@ import serial, csv, os
 def writeHeaders():
     if not file_exists:
         headers = [
-            "ax_rms", "ax_var", "ax_energy", "ax_ptp", "ax_skew", "ax_kurt",
-            "ay_rms", "ay_var", "ay_energy", "ay_ptp", "ay_skew", "ay_kurt",
-            "az_rms", "az_var", "az_energy", "az_ptp", "az_skew", "az_kurt",
-            "gx_rms", "gx_var", "gx_energy", "gx_ptp", "gx_skew", "gx_kurt",
-            "gy_rms", "gy_var", "gy_energy", "gy_ptp", "gy_skew", "gy_kurt",
-            "gz_rms", "gz_var", "gz_energy", "gz_ptp", "gz_skew", "gz_kurt",
+            "ax_rms", "ax_var", "ax_energy", "ax_ptp", "ax_skew", "ax_kurt", "ax_mean",
+            "ay_rms", "ay_var", "ay_energy", "ay_ptp", "ay_skew", "ay_kurt", "ay_mean",
+            "az_rms", "az_var", "az_energy", "az_ptp", "az_skew", "az_kurt", "az_mean",
+            "gx_rms", "gx_var", "gx_energy", "gx_ptp", "gx_skew", "gx_kurt", "gx_mean",
+            "gy_rms", "gy_var", "gy_energy", "gy_ptp", "gy_skew", "gy_kurt", "gx_mean",
+            "gz_rms", "gz_var", "gz_energy", "gz_ptp", "gz_skew", "gz_kurt", "gx_mean",
             "xcorr_ax_ay", "xcorr_ax_az","xcorr_ay_az","xcorr_gx_gy","xcorr_gx_gz","xcorr_gy_gz",
-            "ppg_rms", "ppg_var", "ppg_energy", "ppg_ptp", "ppg_skew", "ppg_kurt",
+            "ppg_rms", "ppg_var", "ppg_energy", "ppg_ptp", "ppg_skew", "ppg_kurt", "ppg_mean",
             "ppg_B0_frac", "ppg_B1_frac", "ppg_B2_frac", "acc_L_frac", "acc_M_frac", "gyro_L_frac","gyro_M_frac",
             "awake"]
         writer.writerow(headers)
